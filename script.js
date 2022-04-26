69a4cbc075f3f369abdc6d41cced974b3fd69c0a
Hector Cardenas <card.hectorl@gmail.com>
2022-04-25 19:25:47 -0500

    Added function that adds book info into a displayed card. Currently only works with one card. Needs logic to seperate book info into separate cards.

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function Book(author, title, numOfPages, hasBeenRead) {
   (this.author = author),
     (this.title = title),
     (this.numOfPages = numOfPages),
-    (this.hasBeenRead = hasBeenRead);
+    (this.hasBeenRead = hasBeenRead),
+    this.info = function () {
+      return title + ' by, ' + author;
+    }
 }
 
 function addBookToLibrary(author, title, numOfPages, hasBeenRead) {
@@ -16,20 +19,58 @@ function addBookToLibrary(author, title, numOfPages, hasBeenRead) {
   myLibrary.push(book);
 }
 
-addBookToLibrary("Suzan Collins", "Catching Fire", 245, true);
+// addBookToLibrary("Suzan Collins", "Catching Fire", 245, true);
 
-addBookToLibrary("Suzan Collins", "Hunger Games", 300, false);
+// addBookToLibrary("Suzan Collins", "Hunger Games", 300, false);
 
 function createBookCard() {
   //Selects the main-section div
   const main = document.querySelector(".main-section");
   //Creates new div element
   const card = document.createElement("div");
-  
+ 
+  //helps to count each separate book .card element
+  let i = 0;
+  if (myLibrary.length>i) {
+    i++;
+  }
   //Styles Card element
-  card.setAttribute('class','card');
-  
+  // card.setAttribute('class','card');
+  card.classList.add('card'+i);
   //Adds card to main-section 
   main.appendChild(card);
 
 }
+
+function addBookToCard() {
+  const card = document.querySelector('.card'+ 1);
+  
+  const h1 = document.createElement('h1');
+  const h2 = document.createElement('h2');
+  const p = document.createElement('h3');
+  const read = document.createElement('h3');
+
+  //adds the title to the card element
+  h1.textContent = myLibrary[myLibrary.length-1].title;
+  h2.textContent = 'by, ' + myLibrary[myLibrary.length-1].author;
+  p.textContent = 'Pages: ' + myLibrary[myLibrary.length-1].numOfPages;
+  read.textContent = 'Has read? ' + myLibrary[myLibrary.length-1].hasBeenRead;
+  card.appendChild(h1);
+  card.appendChild(h2);
+  card.appendChild(p);
+  card.appendChild(read);
+}
+
+
+// myLibrary[0].author --> can be used to access obj elements in array.
+
+// createBookCard();
+// addBookToCard();
+
+function test() {
+// addBookToLibrary("Suzan Collins", "Catching Fire", 245, true);
+  createBookCard();
+  addBookToCard();
+}
+
+//! REMINDER---> You are trying to figure out how to select different cards to put the book info into. Currently it is going into the first .card element. You need each book info in their own .card elements. 
